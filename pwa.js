63cc7cb19a8146be0f8737f1f916dc7802a21c0e
ranasun <ranasun@users.noreply.github.com>
2022-10-24 09:17:00 +0000

    deploy: abc639f65d325de6b7b10a81071b02147107ec79

--- pwa.js
+++
@@ -1,7 +0,0 @@
-// Load service worker for making app installable
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(() => {
-    console.log('Service Worker Registered');
-  });
-}
