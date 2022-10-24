e711a43e4bda805560c8b89b88a95976a9955aff
Randy Asuncion <ranasunc@gmail.com>
2022-10-24 01:37:30 -0700

    PWA

---
+++ public/pwa.js
@@ -0,0 +1,7 @@
+// Load service worker for making app installable
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(() => {
+    console.log('Service Worker Registered');
+  });
+}
---
+++ public/sw.js
@@ -0,0 +1,24 @@
+// Cache files
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('invoice-generator').then((cache) => cache.addAll([
+      'index.html',
+      'pwa.js',
+      'sw.js',
+      './site.webmanifest',
+      './images/android-chrome-192x192.png',
+      './images/apple-touch-icon.png',
+      './images/favicon-32x32.png',
+      './images/android-chrome-512x512.png',
+      './images/favicon-16x16.png',
+      './images/favicon.ico'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
